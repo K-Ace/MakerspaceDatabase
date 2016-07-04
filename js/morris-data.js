61fefcbbcdc1fe7cda62b0e3584712c20f827345
K-Ace <krisfargo@gmail.com>
2016-07-04 10:24:31 -0400

    More updates

--- js/morris-data.js
+++ js/morris-data.js
@@ -4,59 +4,59 @@ $(function() {
         element: 'morris-area-chart',
         data: [{
             period: '2010 Q1',
-            all: 10,
-            mentors: 5,
-            users: 5
+            iphone: 2666,
+            ipad: null,
+            itouch: 2647
         }, {
             period: '2010 Q2',
-            all: 20,
-            mentors: 5,
-            users: 15
+            iphone: 2778,
+            ipad: 2294,
+            itouch: 2441
         }, {
             period: '2010 Q3',
-            all: 27,
-            mentors: 5,
-            users: 22
+            iphone: 4912,
+            ipad: 1969,
+            itouch: 2501
         }, {
             period: '2010 Q4',
-            all: 31,
-            mentors: 5,
-            users: 26
+            iphone: 3767,
+            ipad: 3597,
+            itouch: 5689
         }, {
             period: '2011 Q1',
-            all: 33,
-            mentors: 5,
-            users: 28
+            iphone: 6810,
+            ipad: 1914,
+            itouch: 2293
         }, {
             period: '2011 Q2',
-            all: 35,
-            mentors: 5,
-            users: 30
+            iphone: 5670,
+            ipad: 4293,
+            itouch: 1881
         }, {
             period: '2011 Q3',
-            all: 36,
-            mentors: 5,
-            users: 31
+            iphone: 4820,
+            ipad: 3795,
+            itouch: 1588
         }, {
             period: '2011 Q4',
-            all: 38,
-            mentors: 5,
-            users: 33
+            iphone: 15073,
+            ipad: 5967,
+            itouch: 5175
         }, {
             period: '2012 Q1',
-            all: 40,
-            mentors: 5,
-            users: 35
+            iphone: 10687,
+            ipad: 4460,
+            itouch: 2028
         }, {
             period: '2012 Q2',
-            all: 44,
-            mentors: 5,
-            users: 39
+            iphone: 8432,
+            ipad: 5713,
+            itouch: 1791
         }],
         xkey: 'period',
-        ykeys: ['all', 'mentors', 'users'],
-        labels: ['All', 'Mentors', 'Users'],
-        pointSize: 4,
+        ykeys: ['iphone', 'ipad', 'itouch'],
+        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        pointSize: 2,
         hideHover: 'auto',
         resize: true
     });
@@ -64,54 +64,15 @@ $(function() {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Users",
-            value: 38
-        }, {
-            label: "Volunteers",
+            label: "Download Sales",
             value: 12
         }, {
-            label: "Administrators",
-            value: 5
+            label: "In-Store Sales",
+            value: 30
+        }, {
+            label: "Mail-Order Sales",
+            value: 20
         }],
         resize: true
     });
-
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        hideHover: 'auto',
-        resize: true
-    });
-
 });
