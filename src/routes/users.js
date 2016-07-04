9a7956c96553293d3250cc5798a8b2f21367b3a6
Brandon Smith <brandon.smith100@icloud.com>
2016-07-04 12:25:06 -0400

    Added new javascript stuff

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users');
+});
+
+router.get('/addUser', function(req, res, next) {
+  res.render('adduser');
 });
 
 module.exports = router;
