11d7efca5d2841d08880941f7e8544d8c29b6f64
K-Ace <krisfargo@gmail.com>
2016-07-05 19:51:31 -0400

    test

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users');
-});
-
-router.get('/addUser', function(req, res, next) {
-  res.render('adduser');
+  res.send('respond with a resource');
 });
 
 module.exports = router;
