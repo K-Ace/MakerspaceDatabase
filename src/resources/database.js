bdedbfe69e0dbff9d638bba73b386160bc619e42
K-Ace <krisfargo@gmail.com>
2016-07-15 21:47:23 -0400

    Fixed some connection problems and added the getUsers method.
    Now we gotta do something with that data.

--- src/resources/database.js
+++ src/resources/database.js
@@ -3,10 +3,11 @@
 var database = require('mysql');
 
 var dbConnection = database.createConnection({
-    host : '',
-    user : '',
-    password : 'nicetry',
-    database : ''
+    host : 'makerspace.unh.edu',
+    user : 'web',
+    password : '6kdMgQNiw6XmsnBXmZkVV5AOM8HBMsCg',
+    database : 'database',
+    dateStrings: 'date'
 });
 
 
@@ -15,6 +16,7 @@ function connect()
     dbConnection.connect( function( err ) {
         if(err) {
             console.error("Error connecting: " + err.stack);
+            dbConnection.end();
             return;
         }
         
@@ -24,7 +26,6 @@ function connect()
 
 function addUser(firstName, lastName, email, dateJoined, affiliation, role, rfid)
 {
-    connect();
     
     console.log("Post Attributes: \n\tName: " + firstName + " " + lastName + "\n\tEmail: " + email + "\n\tJoined: " + dateJoined
                + "\n\tAffiliation: " + affiliation + "\n\tRole: " + role + "\n\tRfid: " + rfid);
@@ -35,30 +36,55 @@ function addUser(firstName, lastName, email, dateJoined, affiliation, role, rfid
 			if (err)
                 console.log("Bad Query..." + err);
 			 if (rows.length) {
+                //"Error! User already exists by the name: " + firstName + " " + lastName
+                // Use a jade alert from bootstrap? http://www.w3schools.com/bootstrap/bootstrap_alerts.asp
                 console.log("User already exists");
             } else {
 
-				// if there is no user with that email
+				// if there is no user with that name
                 // create the user
                 var newUserMysql = new Object();
 				
 				newUserMysql.firstName    = firstName;
                 newUserMysql.lastName     = lastName;
                 newUserMysql.email = email; // use the generateHash function in our user model
+
+                //Check that the email is valid, otherwise the databse won't accept the data
 			
-				var insertQuery = "INSERT INTO members ( firstName, lastName, email ) values ('" + firstName + "','" + lastName + "','" + email + "')";
-					console.log(insertQuery);
+				var insertQuery = "INSERT INTO members ( firstName, lastName, email, joinDate ) values ('" + firstName + "','" + lastName + "','" + email + "','" + dateJoined + "')";
+				console.log(insertQuery);
                 
 				dbConnection.query(insertQuery,function(err,rows){
 				    //newUserMysql.id = rows.insertId;
+                    if(err){
+                        console.error(err)
+                    }
+                    //Show success alert at the top of the page if successful
 				});	
+
             }	
 		});
+}
+
+function getUsers()
+{
+    var queryString = 'SELECT firstName, lastName, email, joinDate FROM members';
+
+    console.log(queryString);
+     
+    dbConnection.query(queryString, function(err, rows, fields) {
+        if (err) throw err;            
+       
+        for (var i in rows) {
+            console.log(rows[i])
+        }
+    });
 }
 
 function killConnection()
 {
     dbConnection.end();
+    console.log("Disconnected.");
 }
 
 function getConnection()
@@ -70,3 +96,4 @@ exports.connect = connect;
 exports.killConnection = killConnection;
 exports.getConnection = getConnection;
 exports.addUser = addUser;
+exports.getUsers = getUsers;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 var database = require('../resources/database');
 
-router.get('/', function(req, res, next) {
-  res.render('users');
+router.get('/', function(req, res, next) {  	
+  	database.getUsers();
+  	res.render('users');    
 });
 
 router.get('/addUser', function(req, res, next) {
@@ -12,8 +13,6 @@ router.get('/addUser', function(req, res, next) {
 });
 
 router.post('/addUser', function(req, res, next) {
-    //database.connect();
-
     database.addUser(req.body.firstName, req.body.lastName, req.body.email, req.body.joined, req.body.affiliation, req.body.role, req.body.tagNum);
     res.render('addUser');
 });
